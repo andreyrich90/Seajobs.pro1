21566dab33e29b8a1619c5aeb1a9a0f75fc30e62
andreyrich90 <andreyrich90@gmail.com>
2026-05-30 22:35:59 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "SeaJobs.pro",
+  description: "Maritime career platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
