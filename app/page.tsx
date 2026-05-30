c22d9ad37200d0db886393b023ff45fcddc69366
andreyrich90 <andreyrich90@gmail.com>
2026-05-30 22:37:44 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "40px", textAlign: "center" }}>
+      <h1>SeaJobs.pro</h1>
+      <p>Каркас работает. Скоро здесь будет портал для моряков. ⚓</p>
+    </main>
+  );
+}
